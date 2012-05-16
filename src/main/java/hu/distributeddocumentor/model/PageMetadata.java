a05cea586eb508f41315f5e63ad78ad572713803
Daniel Vigovszky <devnull@localhost>
2012-05-16 12:25:04 +0200

    Fixed issue #43

--- src/main/java/hu/distributeddocumentor/model/PageMetadata.java
+++ src/main/java/hu/distributeddocumentor/model/PageMetadata.java
@@ -1,6 +1,10 @@
 package hu.distributeddocumentor.model;
 
+import com.google.common.base.Joiner;
+import com.google.common.io.Files;
 import java.io.*;
+import java.nio.charset.Charset;
+import java.util.List;
 import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -55,7 +59,8 @@ public class PageMetadata {
     public void save(File root) {
         File metadataFile = getFile(root);
         
-        try {
+        try {            
+            // Writing the file
             OutputStream out = new FileOutputStream(metadataFile);        
             try {
                 metadata.store(out, "Metadata of page " + pageId);
@@ -63,6 +68,12 @@ public class PageMetadata {
             finally {
                 out.close();                    
             }
+            
+            // Removing comments to avoid merging conflicts            
+            final List<String> lines = Files.readLines(metadataFile, Charset.defaultCharset());
+            lines.remove(0);
+            lines.remove(0);
+            Files.write(Joiner.on('\n').join(lines), metadataFile, Charset.defaultCharset());
         }
         catch (IOException ex) {                
             log.error("Failed to load page metadata", ex);
