f32c1d70b4d8de6cc9a33429119862c05fa0fad1
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2013-05-22 22:20:31 +0200

    WikiText added directly to the source control

---
+++ lib/install-local.bat
@@ -0,0 +1,4 @@
+@echo off
+
+cmd /c mvn.bat install:install-file -Dfile=org.eclipse.mylyn.wikitext.core_1.8.0.I20130522-1811.jar -DartifactId=core -DgroupId=org.wikitext -Dversion=1.8.0-SNAPSHOT -Dpackaging=jar
+cmd /c mvn.bat install:install-file -Dfile=org.eclipse.mylyn.wikitext.mediawiki.core_1.8.0.I20130522-1811.jar -DartifactId=mediawiki-core -DgroupId=org.wikitext -Dversion=1.8.0-SNAPSHOT -Dpackaging=jar 
