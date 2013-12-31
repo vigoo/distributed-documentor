00ade59057dab6fedb0a34fa5ec4e743b04a0195
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2013-12-31 15:40:51 +0100

    Fix of issue #79 and removed some unused code

--- src/main/java/hu/distributeddocumentor/gui/JTableWithImagePreview.java
+++ src/main/java/hu/distributeddocumentor/gui/JTableWithImagePreview.java
@@ -3,13 +3,14 @@ package hu.distributeddocumentor.gui;
 import java.awt.Dimension;
 import java.awt.event.MouseEvent;
 import java.io.File;
+import javax.swing.ImageIcon;
 import javax.swing.JTable;
 
 
 public class JTableWithImagePreview extends JTable {
 
     private File root;
-    private Dimension maxToolTipSize = new Dimension(200, 200);
+    private Dimension maxToolTipSize = new Dimension(300, 300);
 
     public File getRoot() {
         return root;
@@ -37,7 +38,16 @@ public class JTableWithImagePreview extends JTable {
             String name = (String)getValueAt(index, 0);
             File img = new File(root, name);
             
-            return "<html><body><h3>" + name + "</h3><img src='" + img.toURI() + "' width='"+ Integer.toString(maxToolTipSize.width) + "' height='" + Integer.toString(maxToolTipSize.height) + "'></body></html>";
+            ImageIcon icon = new ImageIcon(img.getAbsolutePath());
+            double horizontalPercentage = (double)maxToolTipSize.width / (double)icon.getIconWidth();
+            double verticalPercentage = (double)maxToolTipSize.height / (double)icon.getIconHeight();
+            double percentage = Math.min(1.0, Math.min(horizontalPercentage, verticalPercentage));
+            
+            int targetWidth = (int)Math.round((double)icon.getIconWidth() * percentage);
+            int targetHeight = (int)Math.round((double)icon.getIconHeight() * percentage);
+            
+            String html = "<html><body><h3>" + name + "</h3><img src='" + img.toURI() + "' width='"+ Integer.toString(targetWidth) + "' height='" + Integer.toString(targetHeight) + "'></body></html>";
+            return html;
         }
         
         return null;
