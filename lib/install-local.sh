06d3fcc3e9a83f8dbcef6c33a6da992fc1a965cb
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2014-08-28 21:05:41 +0200

    Updated dependency install script

--- lib/install-local.sh
+++ lib/install-local.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-mvn install:install-file -Dfile=org.eclipse.mylyn.wikitext.core_1.8.0.I20130522-1811.jar -DartifactId=core -DgroupId=org.wikitext -Dversion=1.8.0-SNAPSHOT -Dpackaging=jar
-mvn install:install-file -Dfile=org.eclipse.mylyn.wikitext.mediawiki.core_1.8.0.I20130522-1811.jar -DartifactId=mediawiki-core -DgroupId=org.wikitext -Dversion=1.8.0-SNAPSHOT -Dpackaging=jar 
+ROOT=`dirname $0`
+mvn install:install-file -Dfile=$ROOT/org.eclipse.mylyn.wikitext.core_1.8.0.I20130522-1811.jar -DartifactId=core -DgroupId=org.wikitext -Dversion=1.8.0-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=$ROOT/org.eclipse.mylyn.wikitext.mediawiki.core_1.8.0.I20130522-1811.jar -DartifactId=mediawiki-core -DgroupId=org.wikitext -Dversion=1.8.0-SNAPSHOT -Dpackaging=jar 
